808a68b28b45e8dd07f12675b53151fe47252125
Gowthamm18 <113981550+Gowthamm18@users.noreply.github.com>
2024-09-04 21:29:04 +0530

    Add files via upload

---
+++ Responsive/portfolio.js
@@ -0,0 +1,5 @@
+let contactMeEl=document.getElementById("contactMeButton");
+
+contactMeEl.onclick=function() {
+    location.href="https://res-console.cloudinary.com/dci7l2rg7/thumbnails/v1/image/upload/v1725464328/UmVzdW1lLkdvd3RoYW1hZGlzaHRpXzJfd2pwanJx/drilldown";
+ }
