8cd0d8cbf800a6d3483344a0ae402f0b37d50a92
Gowthamm18 <113981550+Gowthamm18@users.noreply.github.com>
2024-09-05 13:21:54 +0530

    portfolio.js

--- Responsive/portfolio.js
+++ Responsive/portfolio.js
@@ -1,5 +1,5 @@
 let contactMeEl=document.getElementById("contactMeButton");
 
 contactMeEl.onclick=function() {
-    location.href="https://res-console.cloudinary.com/dci7l2rg7/thumbnails/v1/image/upload/v1725464328/UmVzdW1lLkdvd3RoYW1hZGlzaHRpXzJfd2pwanJx/drilldown";
+    location.href="https://res.cloudinary.com/dci7l2rg7/image/upload/v1725520266/Resume.Gowthamadishti_2__page-0001_tksnaz.jpg";
  }
